d1a8fb11435885cb8c90d2e69c2b719e9c40af39
wrk-123 <wrk123@qq.com>
2024-11-15 01:26:08 +0800

    edited output.cpp,created html

--- src/output.cpp
+++ src/output.cpp
@@ -1,4 +1,11 @@
 #include "maj.h"
 
-void output()
-{}
+void output(){
+    for(auto player:players){
+        std::string outputpath="./html/";
+        outputpath+=player.name+".html";
+        std::cerr<<outputpath<<'\n';
+        std::ofstream cout(outputpath.c_str(),std::ios_base::out);
+        cout<<player.name<<'\n';
+    }
+}
