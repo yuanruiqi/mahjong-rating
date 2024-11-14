603ea81a1e9178e42bc9b3a85bdf8aafaf92d5bf
wrk-123 <wrk123@qq.com>
2024-11-15 00:50:12 +0800

    change std::cin to ifstream in input.cpp.

--- src/input.cpp
+++ src/input.cpp
@@ -3,11 +3,12 @@
 const std::string events_file="events.txt";
 const std::string users_file="username.txt";
 void read_users(){
-    freopen(users_file.c_str(),"r",stdin);
+    std::ifstream cin;
+    cin.open(users_file.c_str());
     i32 maxid=0;
     i32 id;std::string name;
     std::vector<std::pair<i32,std::string>>zz;
-    while(std::cin>>id>>name){
+    while(cin>>id>>name){
         zz.push_back({id,name});
         maxid=std::max(id,maxid);
 
@@ -21,26 +22,27 @@ void read_users(){
     // fclose(stdin);
 }
 void read_events(){
-    freopen(events_file.c_str(),"r",stdin);
+    std::ifstream cin;
+    cin.open(events_file.c_str());
     i32 n;
-    while(std::cin>>n){
+    while(cin>>n){
         event crc;
         i32 ida,idb,idc,idd;
-        std::cin>>ida>>idb>>idc>>idd;
+        cin>>ida>>idb>>idc>>idd;
         crc.per[0]=ida;
         crc.per[1]=idb;
         crc.per[2]=idc;
         crc.per[3]=idd;
         for(i32 i=1;i<=n;i++){
             game p;
-            std::cin>>p.host;
+            cin>>p.host;
             for(i32 j=0;j<=3;j++){
                 i32 id;
-                std::cin>>id;
+                cin>>id;
                 p.stat[j]=(state)id;
             }
             for(i32 j=0;j<=3;j++){
-                std::cin>>p.delta[j];
+                cin>>p.delta[j];
             }
             crc.games.push_back(p);
         }
