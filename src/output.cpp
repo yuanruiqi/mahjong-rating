791b05ed0f1eee5b76ab4008bce341f7eaee7d0b
wrk-123 <wrk123@qq.com>
2024-11-15 15:48:33 +0800

    Add index.html

--- src/output.cpp
+++ src/output.cpp
@@ -1,6 +1,7 @@
 #include "maj.h"
-
+const std::string indexlink="./html/index.html";
 void output(){
+    std::string opt="";
     for(auto player:players){
         std::string outputpath="./html/";
         outputpath+=player.name+".html";
@@ -18,5 +19,8 @@ void output(){
         cout<<"rating: "<<std::fixed<<' '<<std::setprecision(3)<<player.rating<<'\n';
         cout<<"放铳次数："<<player.crush_cnt<<'\n';
         cout<<"平均荣/自摸点："<<std::fixed<<' '<<std::setprecision(3)<<player.win_sum*1.0/std::max(1ll,player.win_cnt)<<'\n';
+        opt+="<p><a href=\"./"+player.name+".html\">"+player.name+"</a></p>";
     }
+        std::ofstream cout(indexlink.c_str(),std::ios_base::out);
+        cout<<opt;
 }
