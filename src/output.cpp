6efb8dd25ab1165aac33b65e4b358b2a92a7d173
wrk-123 <wrk123@qq.com>
2024-11-15 16:00:01 +0800

    Add more information

--- src/output.cpp
+++ src/output.cpp
@@ -8,18 +8,19 @@ void output(){
         std::cerr<<outputpath<<'\n';
         std::ofstream cout(outputpath.c_str(),std::ios_base::out);
         cout<<"<meta charset=\"utf-8\">\n";
-        cout<<"姓名："<<player.name<<'\n';
+        cout<<"<p>姓名："<<player.name<<"</p>\n";
         i32 t=0;
         for(i32 i=0;i<=4;i++){
             if(i==4)cout<<"被飞";
             else cout<<i+1<<"位";
-            cout<<"率: "<<std::fixed<<' '<<std::setprecision(3)<<player.rank_cnt[i]*1.0/std::max(1ll,player.event_cnt)<<'\n';
+            cout<<"率: "<<std::fixed<<' '<<std::setprecision(3)<<player.rank_cnt[i]*1.0/std::max(1ll,player.event_cnt)<<"</p>\n";
             t+=std::min(i+1,4)*player.rank_cnt[i];
         }
-        cout<<"平均顺位："<<std::fixed<<' '<<std::setprecision(3)<<t*1.0/std::max(1ll,player.event_cnt)<<'\n';
-        cout<<"rating: "<<std::fixed<<' '<<std::setprecision(3)<<player.rating<<'\n';
-        cout<<"放铳次数："<<player.crush_cnt<<'\n';
-        cout<<"平均荣/自摸点："<<std::fixed<<' '<<std::setprecision(3)<<player.win_sum*1.0/std::max(1ll,player.win_cnt)<<'\n';
+        cout<<"<p>平均顺位："<<std::fixed<<' '<<std::setprecision(3)<<t*1.0/std::max(1ll,player.event_cnt)<<"</p>\n";
+        cout<<"<p>rating: "<<std::fixed<<' '<<std::setprecision(3)<<player.rating<<"</p>\n";
+        cout<<"<p>放铳概率："<<std::fixed<<' '<<std::setprecision(3)<<player.crush_cnt*1.0/std::max(1ll,player.game_cnt)<<"</p>\n";
+        cout<<"<p>平均打点："<<std::fixed<<' '<<std::setprecision(3)<<player.win_sum*1.0/std::max(1ll,player.win_cnt)<<"</p>\n";
+        cout<<"<p>参加局数："<<player.event_cnt<<"</p>\n";
         opt+="<p><a href=\"./"+player.name+".html\">"+player.name+"</a></p>";
     }
         std::ofstream cout(indexlink.c_str(),std::ios_base::out);
