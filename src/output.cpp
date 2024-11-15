4370ce37bf093eb9a4280ac9b32e8b877e85800d
wrk-123 <wrk123@qq.com>
2024-11-15 12:29:22 +0800

    Show more information

--- src/output.cpp
+++ src/output.cpp
@@ -16,5 +16,7 @@ void output(){
         }
         cout<<"平均顺位："<<std::fixed<<' '<<std::setprecision(3)<<t*1.0/std::max(1ll,player.event_cnt)<<'\n';
         cout<<"rating: "<<std::fixed<<' '<<std::setprecision(3)<<player.rating<<'\n';
+        cout<<"放铳次数："<<player.crush_cnt<<'\n';
+        cout<<"平均荣/自摸点："<<std::fixed<<' '<<std::setprecision(3)<<player.win_sum*1.0/std::max(1ll,player.win_cnt)<<'\n';
     }
 }
