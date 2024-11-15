e1c20f2e385f3e8a001c424f600841204df1a311
Ruiqi Yuan <yuanruiqi@qq.com>
2024-11-15 12:39:55 +0800

    Add ./data

--- src/input.cpp
+++ src/input.cpp
@@ -1,7 +1,7 @@
 #include "maj.h"
 
-const std::string events_file="events.txt";
-const std::string users_file="username.txt";
+const std::string events_file="./data/events.txt";
+const std::string users_file="./data/username.txt";
 void read_users(){
     std::ifstream cin;
     cin.open(users_file.c_str());
